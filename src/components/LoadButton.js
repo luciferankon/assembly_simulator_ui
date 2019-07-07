2e11116c4af2955af1ab419ebba0281740b201f1
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-07-07 18:16:20 +0530

    Change Dunston file extension from .txt to .dtn (#4)

--- src/components/LoadButton.js
+++ src/components/LoadButton.js
@@ -1,48 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { DUNSTON_FILE_EXTENSION } from '../constants';
 
 class LoadButton extends Component {
-    constructor(props) {
-        super(props);
-        this.fileReader = new FileReader();
-        this.fileInputRef = React.createRef();
-        this.handleFileChosen = this.handleFileChosen.bind(this);
-        this.handleLoadClick = this.handleLoadClick.bind(this);
-        this.handleFileRead = this.handleFileRead.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.fileReader = new FileReader();
+    this.fileInputRef = React.createRef();
+    this.handleFileChosen = this.handleFileChosen.bind(this);
+    this.handleLoadClick = this.handleLoadClick.bind(this);
+    this.handleFileRead = this.handleFileRead.bind(this);
+  }
 
-    handleFileRead(){
-        const fileContent = this.fileReader.result;
-        this.props.handleCodeEdit(fileContent);
-    }
+  handleFileRead() {
+    const fileContent = this.fileReader.result;
+    this.props.handleCodeEdit(fileContent);
+  }
 
-    handleFileChosen(e){
-        const file = e.target.files[0];
-        this.fileReader.onloadend = this.handleFileRead;
-        this.fileReader.readAsText(file);
-    }
+  handleFileChosen(e) {
+    const file = e.target.files[0];
+    this.fileReader.onloadend = this.handleFileRead;
+    this.fileReader.readAsText(file);
+  }
 
-    handleLoadClick() {
-        this.fileInputRef.current.click();
-    }
+  handleLoadClick() {
+    this.fileInputRef.current.click();
+  }
 
-    render() {
-        return (
-            <div>
-                <input
-                    type="file"
-                    style={{ display: "none" }}
-                    ref={this.fileInputRef}
-                    onChange={this.handleFileChosen}
-                />
-                <a
-                    className={this.props.className}
-                    onClick={this.handleLoadClick}
-                >
-                    Load
-                </a>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input
+          type="file"
+          style={{ display: 'none' }}
+          ref={this.fileInputRef}
+          onChange={this.handleFileChosen}
+          accept={DUNSTON_FILE_EXTENSION}
+        />
+        <a className={this.props.className} onClick={this.handleLoadClick}>
+          Load
+        </a>
+      </div>
+    );
+  }
 }
 
 export default LoadButton;
