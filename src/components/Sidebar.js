6d8815ce8b56c81c7b3c2ee1cf8b4658bffe12c1
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-07-13 16:35:19 +0530

    Add feature to close sidebar when clicked outside of sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import SidebarItemWithSlider from './SidebarItemWithSlider';
 
-class Sidebar extends Component {
-  render() {
+const Sidebar = (props) => {
     const className =
-      this.props.className + (this.props.isOpened ? '' : ' sidebar-closed');
+      props.className + (props.isOpened ? '' : ' sidebar-closed');
     return (
-      <div className={className}>
-        <SidebarItemWithSlider title={"Max Instructions"} min={100} max={10000} sliderRef={this.props.sliderRef} />
+      <div className={className} ref={props.sidebarRef}>
+        <SidebarItemWithSlider title={"Max Instructions"} min={100} max={100000} sliderRef={props.sliderRef} />
       </div>
     );
-  }
 }
 
 export default Sidebar;
